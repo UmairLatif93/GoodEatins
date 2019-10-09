596198cde8536eafeb370fc0eff8e3e0c3f02d9e
Umair Latif <m.umair.latif1@gmail.com>
2019-10-09 14:44:45 +0500

    Complete Project

---
+++ Good Eatins/Controller/RecipesSelectionVC.swift
@@ -0,0 +1,30 @@
+//
+//  RecipesSelectionVC.swift
+//  Good Eatins
+//
+//  Created by Umair Latif on 07/10/2019.
+//  Copyright © 2019 Umair Latif. All rights reserved.
+//
+
+import UIKit
+
+class RecipesSelectionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Good Eatins/View/RecipeCell.swift
@@ -0,0 +1,13 @@
+//
+//  RecipeCell.swift
+//  Good Eatins
+//
+//  Created by Umair Latif on 08/10/2019.
+//  Copyright © 2019 Umair Latif. All rights reserved.
+//
+
+import UIKit
+
+class RecipeCell: UICollectionViewCell {
+    
+}
